1681f863d08ba9bde59a153c4fbfa355283ee335
Georgios Andreadis <info@gandreadis.com>
2019-05-24 16:58:00 +0200

    Add progress reporter

---
+++ evopy/progress_report.py
@@ -0,0 +1,13 @@
+class ProgressReport:
+    """Class representing a report on an intermediate state of the learning process."""
+
+    def __init__(self, generation, best_genotype, best_fitness):
+        """Initializes the report instance.
+        
+        :param generation: number identifying the reported generation
+        :param best_genotype: the genotype of the best individual of that generation
+        :param best_fitness: the fitness of the best individual of that generation
+        """
+        self.generation = generation
+        self.best_genotype = best_genotype
+        self.fitness = best_fitness
--- test/test_evopy.py
+++ test/test_evopy.py
@@ -2,7 +2,21 @@
 from evopy import EvoPy
 
 
-def simple_test():
+def test_simple_use_case():
     """Test whether evopy can successfully run for a simple evaluation function."""
     evopy = EvoPy(lambda x: pow(x, 2), 1)
     evopy.run()
+
+
+def test_progress_reporting():
+    """Test whether all generations are reported."""
+    count = [0]
+
+    def reporter(progress_report):
+        assert progress_report.generation == count[0]
+        count[0] += 1
+
+    evopy = EvoPy(lambda x: pow(x, 2), 1, reporter=reporter)
+    evopy.run()
+
+    assert count[0] == 100
