524cdf15fda2831bbd732025d340913b9ddb3417
Georgios Andreadis <info@gandreadis.com>
2019-05-24 17:02:42 +0200

    Fix lint errors

--- evopy/progress_report.py
+++ evopy/progress_report.py
@@ -1,9 +1,12 @@
+"""Module containing the ProgressReport class, used to report on the progress of the optimizer."""
+
+
 class ProgressReport:
     """Class representing a report on an intermediate state of the learning process."""
 
     def __init__(self, generation, best_genotype, best_fitness):
         """Initializes the report instance.
-        
+
         :param generation: number identifying the reported generation
         :param best_genotype: the genotype of the best individual of that generation
         :param best_fitness: the fitness of the best individual of that generation
