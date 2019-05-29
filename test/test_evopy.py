89398e38b2f626ee6fd73cd58e188388d0ff418b
Georgios Andreadis <info@gandreadis.com>
2019-05-29 13:05:32 +0200

    Adds two usage examples to the README (#15)
    * Adds two usage examples to the README
    
    Also adds a more complex 2D example to the smoketest-suite.
    
    * Move installation back up
    
    * Code block

--- test/test_evopy.py
+++ test/test_evopy.py
@@ -1,4 +1,6 @@
 """End to end tests for evopy."""
+import numpy as np
+
 from evopy import EvoPy
 
 
@@ -6,6 +8,14 @@ def test_simple_use_case():
     """Test whether evopy can successfully run for a simple evaluation function."""
     evopy = EvoPy(lambda x: pow(x, 2), 1)
     evopy.run()
+
+
+def test_multi_dimensional_use_case():
+    """Test whether evopy can successfully run for a multi-dimensional evaluation function
+    (the Rastrigin function)."""
+    evopy = EvoPy(lambda X: 5 + sum([(x ** 2 - 5 * np.cos(2 * np.pi * x)) for x in X]), 2,
+                  generations=1000, population_size=100)
+    print(evopy.run())
 
 
 def test_progress_reporting():
